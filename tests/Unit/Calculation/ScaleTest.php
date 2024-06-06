874fdbee760a971e83b0ab4a657effb9d999069a
tetthys <jeongwoo0830z@gmail.com>
2024-06-06 12:52:16 +0900

    add a ScaleTest.php

---
+++ tests/Unit/Calculation/ScaleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Tetthys\Bc\Bc;
+
+describe('ScaleTest', function () {
+    it('uses the default scale value of 0', function () {
+        $result = (new Bc)->num('1')->add('2')->value();
+        expect($result)->toBe('3');
+    });
+
+    it('chains scale value', function () {
+        $result = (new Bc)->num('1')->add('2')->scale(2)->mul('3')->value();
+        expect($result)->toBe('9.00');
+    });
+});
